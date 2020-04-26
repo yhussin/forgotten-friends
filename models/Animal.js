eaa68433790bc506edbd9b674e1bc41eadd54fa0
Julian1313 <jochoa1011@gmail.com>
2020-04-26 15:10:23 -0700

    adds controllers

---
+++ models/Animal.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const AnimalSchema = new mongoose.Schema({
+    name: {type: String},
+    age: {type: Number}
+})
+
+const Animal = mongoose.model('Animal', AnimalSchema);
+module.exports = Animal;
