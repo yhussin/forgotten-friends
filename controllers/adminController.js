32992a5cb8f05e9b55fdf32a3e9100f2655d2a1d
yhussin <yhussin@gmail.com>
2020-04-27 14:53:34 -0700

    adds register login

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+// Database
+const db = require('../models');
+
+
 router.get('/', (req, res) => {
     res.render('admin')
 });
@@ -11,7 +15,27 @@ router.get('/register', (req, res) => {
 
 router.post('/register', async (req, res) => {
     console.log(req.body)
-})
+
+    try {
+        const admin = await db.Admin.findOne({username: req.body.username});
+
+        if (admin) {
+            return res.send('<h1>Account already exists, try again</h1>')
+        }
+    
+    const adminData = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    }
+
+    await db.Admin.create(adminData);
+    res.redirect('/admin/login');
+    } catch (err) {
+        res.send(err)
+        console.log('err', err)
+    }
+});
 
 router.get('/login', (req, res) => {
     res.render('admin/login')
