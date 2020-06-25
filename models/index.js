cf578d0618002bbad7fb30f7b1c207bd4d66ed3e
yhussin <yhussin@gmail.com>
2020-06-25 09:59:17 -0700

    deploy'

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connectionString = 'mongodb://localhost:27017/forgotten-friends'
+const connectionString = process.env.MONGODB_URI || 'mongodb+srv://yousef:BVI8rBstmZkaH9s6@cluster0-lazji.mongodb.net/forgotten-friends?retryWrites=true&w=majority'
 
 const configOptions = {
     useNewUrlParser: true,
